f9265340dbab134e13164fca9ad67e359a579914
Revanth <revanthcse1@gmail.com>
2024-08-26 01:53:00 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+// Variables
+
+const accountId = 123432;
+
+let accountEmail = 'test@gmail.com';
+
+var accountPassword = '12345';
+
+accountCity ='JFK';
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
