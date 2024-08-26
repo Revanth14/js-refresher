150e0a8b1cf664ecb35567819b74f024bbc3782e
Revanth <revanthcse1@gmail.com>
2024-08-26 03:15:27 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// alert(2+3) 
+
+let name = 'Revanth'
+let age = 25
+let loggedIn = true
+
+// Some of the datatypes are
+/*
+number
+bigint
+string
+boolean
+null
+undefined
+symbol --> uniqueness
+*/
+
+console.log(typeof "Revanth")
+console.log(typeof null)
+console.log(typeof undefined)
